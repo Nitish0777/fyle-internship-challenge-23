361530ee7dcb4092ebb377e43b3c1918ff2e1c1a
Nitish Nashine <nitishnashine@gmail.com>
2024-04-13 13:25:48 +0530

    Refactor API service to include caching for user data and repositories

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, map, tap, throwError } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs'; // Import throwError
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -8,25 +9,47 @@ import { catchError, map, tap, throwError } from 'rxjs';
 export class ApiService {
   constructor(private httpClient: HttpClient) {}
 
-  getUser(githubUsername: string) {
-    return this.httpClient
-      .get(`https://api.github.com/users/${githubUsername}`)
-      .pipe(
-        tap(() => console.log('User fetched successfully')),
+  // Define a cache object to store cached data
+  private cache: { [key: string]: any } = {};
+
+  getUser(githubUsername: string): Observable<any> {
+    const url = `https://api.github.com/users/${githubUsername}`;
+
+    // Check if data is available in cache
+    if (this.cache[url]) {
+      // Return cached data as an observable
+      return of(this.cache[url]);
+    } else {
+      // Fetch data from API and cache it
+      return this.httpClient.get(url).pipe(
+        tap((user) => {
+          // Cache the fetched data
+          this.cache[url] = user;
+          console.log('User fetched successfully');
+        }),
         catchError(this.handleError)
       );
+    }
   }
 
-  // implement getRepos method by referring to the documentation. Add proper types for the return type and params
-  getRepos(githubUsername: string, page: number, perPage: number) {
-    return this.httpClient
-      .get(
-        `https://api.github.com/users/${githubUsername}/repos?page=${page}&per_page=${perPage}`
-      )
-      .pipe(
-        tap((response) => console.log('Repos fetched successfully', response)),
+  getRepos(
+    githubUsername: string,
+    page: number,
+    perPage: number
+  ): Observable<any> {
+    const url = `https://api.github.com/users/${githubUsername}/repos?page=${page}&per_page=${perPage}`;
+
+    if (this.cache[url]) {
+      return of(this.cache[url]);
+    } else {
+      return this.httpClient.get(url).pipe(
+        tap((repos) => {
+          this.cache[url] = repos;
+          console.log('Repos fetched successfully', repos);
+        }),
         catchError(this.handleError)
       );
+    }
   }
 
   private handleError(error: any) {
